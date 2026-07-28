b5b37d872f0ccd425cad772288082c7e0fea719c
Andrew Boylan <andreweboylan@gmail.com>
2026-07-28 16:52:38 -0400

    Display build time in Eastern Time

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,9 @@ readonly REPOSITORY_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 cd "${REPOSITORY_DIR}"
 git pull --ff-only origin main
 
-readonly FLOW_COLLATOR_BUILD_TIME="$(date -u "+%Y-%m-%dT%H:%M:%SZ")"
+readonly FLOW_COLLATOR_BUILD_TIME="$(
+  TZ=America/New_York date "+%Y-%m-%d %I:%M:%S %p %Z"
+)"
 readonly FLOW_COLLATOR_COMMIT_SHA="$(git rev-parse --short=12 HEAD)"
 export FLOW_COLLATOR_BUILD_TIME
 export FLOW_COLLATOR_COMMIT_SHA
